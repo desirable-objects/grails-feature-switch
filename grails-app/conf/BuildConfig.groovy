ec7a0d06de0849eef99f3ca4976c75a9af40e019
Rob Fletcher <robert.w.fletcher@gmail.com>
2012-08-21 11:32:40 +0100

    @Ant @Matt @Tomas Added SCM url and initial documentation.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -4,6 +4,8 @@ grails.project.test.reports.dir = "target/test-reports"
 grails.project.target.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
+grails.release.scm.enabled = false
+
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
